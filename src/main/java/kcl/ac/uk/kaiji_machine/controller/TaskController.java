259cc5b6d75e86a2abdfa7d2dbb48ccc6972368d
hechen <malfuriionzhaofour@gmail.com>
2023-01-29 17:38:57 +0000

    fix

--- src/main/java/kcl/ac/uk/kaiji_machine/controller/TaskController.java
+++ src/main/java/kcl/ac/uk/kaiji_machine/controller/TaskController.java
@@ -1,13 +1,19 @@
 package kcl.ac.uk.kaiji_machine.controller;
 
+import kcl.ac.uk.kaiji_machine.dao.Task;
 import kcl.ac.uk.kaiji_machine.facade.TaskServiceFacadeImpl;
 import kcl.ac.uk.kaiji_machine.response.BaseResult;
+import kcl.ac.uk.kaiji_machine.response.HandleResult;
+import kcl.ac.uk.kaiji_machine.util.PropertiesUtils;
 import kcl.ac.uk.kaiji_machine.vo.TaskVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * @author He Chen
@@ -23,7 +29,7 @@ public class TaskController {
 
     @RequestMapping(value = "/update-cron", method = RequestMethod.POST)
     @ResponseBody
-    public BaseResult updateTaskCron(TaskVO taskVO) throws Exception {
+    public BaseResult updateTaskCron(@RequestBody TaskVO taskVO) throws Exception {
 
        taskServiceFacade.updateTaskCron(taskVO);
 
@@ -32,7 +38,7 @@ public class TaskController {
 
     @RequestMapping(value = "/shutdown", method = RequestMethod.POST)
     @ResponseBody
-    public BaseResult stopTask(TaskVO taskVO) throws Exception {
+    public BaseResult stopTask(@RequestBody TaskVO taskVO) throws Exception {
 
        taskServiceFacade.stopTask(taskVO);
 
@@ -41,10 +47,20 @@ public class TaskController {
 
     @RequestMapping(value = "/boot", method = RequestMethod.POST)
     @ResponseBody
-    public BaseResult startTask(TaskVO taskVO) throws Exception {
+    public BaseResult startTask(@RequestBody TaskVO taskVO) throws Exception {
 
         taskServiceFacade.startTask(taskVO);
 
         return new BaseResult().setCode("100").setDesc("success");
     }
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    @ResponseBody
+    public HandleResult<List<Task>> queryAllTask() {
+
+        List<Task> tasks = taskServiceFacade.queryAllTask();
+
+        return new HandleResult<List<Task>>().setCode("100").setDesc("success").setData(tasks);
+    }
+
 }
