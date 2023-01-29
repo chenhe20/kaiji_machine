a324166e7f3f903f080ad48ced22e24e0ecd8308
hechen <malfuriionzhaofour@gmail.com>
2023-01-29 17:37:53 +0000

    enables cros

---
+++ src/main/java/kcl/ac/uk/kaiji_machine/config/WebConfig.java
@@ -0,0 +1,25 @@
+package kcl.ac.uk.kaiji_machine.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author He Chen
+ * @university King's College London
+ * @ID 21044375
+ */
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+
+
+}
