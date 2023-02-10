b9be69c1408e0c8303dc9613f2900778bafd12bc
hechen <malfuriionzhaofour@gmail.com>
2023-02-10 20:28:03 +0000

    Wraps return results

--- src/main/java/kcl/ac/uk/kaiji_machine/controller/ExcgRateController.java
+++ src/main/java/kcl/ac/uk/kaiji_machine/controller/ExcgRateController.java
@@ -1,13 +1,13 @@
 package kcl.ac.uk.kaiji_machine.controller;
 
 import kcl.ac.uk.kaiji_machine.dao.ExcgRate;
+import kcl.ac.uk.kaiji_machine.response.HandleResult;
 import kcl.ac.uk.kaiji_machine.service.impl.ExcgRateServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -17,16 +17,19 @@ import java.util.List;
  * @ID 21044375
  */
 @Controller
-@RequestMapping("/info-query")
-public class QueryController {
+@RequestMapping("/excgrate")
+public class ExcgRateController {
 
     @Autowired
     ExcgRateServiceImpl excgRateService;
 
-    @RequestMapping(value = "/excgRate", method = RequestMethod.GET)
+    @RequestMapping(value = "/recent", method = RequestMethod.GET)
     @ResponseBody
-    public List<ExcgRate> queryExcgRate() {
-        return excgRateService.queryRecentExcgRate();
+    public HandleResult<List<ExcgRate>> queryExcgRate() {
+
+        List<ExcgRate> excgRates = excgRateService.queryRecentExcgRate();
+
+        return new HandleResult<List<ExcgRate>>().setData(excgRates).setCode("100").setDesc("success");
     }
 
 }
