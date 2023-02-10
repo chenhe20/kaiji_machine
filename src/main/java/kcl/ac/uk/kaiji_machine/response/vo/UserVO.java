89742a7fac990da1658f98f2d1688f43e5b9a60a
hechen <malfuriionzhaofour@gmail.com>
2023-02-10 20:46:28 +0000

    Adds User vo

---
+++ src/main/java/kcl/ac/uk/kaiji_machine/response/vo/UserVO.java
@@ -0,0 +1,16 @@
+package kcl.ac.uk.kaiji_machine.response.vo;
+
+import lombok.Data;
+
+/**
+ * @author He Chen
+ * @university King's College London
+ * @ID 21044375
+ */
+
+@Data
+public class UserVO {
+    String username;
+    Integer id;
+    String createdTime;
+}
