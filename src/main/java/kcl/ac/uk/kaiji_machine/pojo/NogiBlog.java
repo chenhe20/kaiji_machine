8e95f50a4dac4d33c637d31ea37c357d4559e3e0
hechen <malfuriionzhaofour@gmail.com>
2023-02-10 20:44:44 +0000

    Used as object prototype of JSON String returned by nogizaka external api

---
+++ src/main/java/kcl/ac/uk/kaiji_machine/pojo/NogiBlog.java
@@ -0,0 +1,24 @@
+package kcl.ac.uk.kaiji_machine.pojo;
+
+import lombok.Data;
+
+/**
+ * @author He Chen
+ * @university King's College London
+ * @ID 21044375
+ */
+
+@Data
+public class NogiBlog {
+
+    String code;
+    String title;
+    String text;
+    String img;
+    String date;
+    String link;
+    String arti_code;
+    String artist_img;
+    String name;
+
+}
