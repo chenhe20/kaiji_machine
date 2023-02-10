23ad09a45e16cb04b65dd24512bf6ec7648b9e28
hechen <malfuriionzhaofour@gmail.com>
2023-02-10 20:30:08 +0000

    dto class name change

--- src/main/java/kcl/ac/uk/kaiji_machine/controller/TaskController.java
+++ src/main/java/kcl/ac/uk/kaiji_machine/controller/TaskController.java
@@ -4,8 +4,7 @@ import kcl.ac.uk.kaiji_machine.dao.Task;
 import kcl.ac.uk.kaiji_machine.facade.TaskServiceFacadeImpl;
 import kcl.ac.uk.kaiji_machine.response.BaseResult;
 import kcl.ac.uk.kaiji_machine.response.HandleResult;
-import kcl.ac.uk.kaiji_machine.util.PropertiesUtils;
-import kcl.ac.uk.kaiji_machine.vo.TaskVO;
+import kcl.ac.uk.kaiji_machine.dto.TaskDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,27 +28,27 @@ public class TaskController {
 
     @RequestMapping(value = "/update-cron", method = RequestMethod.POST)
     @ResponseBody
-    public BaseResult updateTaskCron(@RequestBody TaskVO taskVO) throws Exception {
+    public BaseResult updateTaskCron(@RequestBody TaskDto taskDto) throws Exception {
 
-       taskServiceFacade.updateTaskCron(taskVO);
+       taskServiceFacade.updateTaskCron(taskDto);
 
         return new BaseResult().setCode("100").setDesc("success");
     }
 
     @RequestMapping(value = "/shutdown", method = RequestMethod.POST)
     @ResponseBody
-    public BaseResult stopTask(@RequestBody TaskVO taskVO) throws Exception {
+    public BaseResult stopTask(@RequestBody TaskDto taskDto) throws Exception {
 
-       taskServiceFacade.stopTask(taskVO);
+       taskServiceFacade.stopTask(taskDto);
 
         return new BaseResult().setCode("100").setDesc("success");
     }
 
     @RequestMapping(value = "/boot", method = RequestMethod.POST)
     @ResponseBody
-    public BaseResult startTask(@RequestBody TaskVO taskVO) throws Exception {
+    public BaseResult startTask(@RequestBody TaskDto taskDto) throws Exception {
 
-        taskServiceFacade.startTask(taskVO);
+        taskServiceFacade.startTask(taskDto);
 
         return new BaseResult().setCode("100").setDesc("success");
     }
